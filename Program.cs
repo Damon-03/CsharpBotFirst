50f5e2928d327b3b1964b187ec02be0fb91f52fa
Damon-03 <73558661+Damon-03@users.noreply.github.com>
2021-01-11 17:47:18 +1300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace PlaceHolder
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+        /*
+        public static void Main(string[] args) 
+            => new Program().MainAsync.GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            
+            string token = "Nzk3OTExMzMwNDc1OTk5MjU0.X_tW1Q.rUtewSD1E7SybZbXqOC7ucaERuE";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        */
+    }
+}
